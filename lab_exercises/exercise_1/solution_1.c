4e39295a1180d089363c1eb619e861a498421e76
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-02-20 22:18:12 +0200

    removed the executable file and modified the .c file to add writing to a file

--- lab_exercises/exercise_1/solution_1.c
+++ lab_exercises/exercise_1/solution_1.c
@@ -3,11 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char *argv[]){
     int status;
     pid_t child;
     child = fork();
+
+    const char *buf = "Test\n"; //to periexomeno pou tha grapsei sto arxeio
+    
 
     if (child<0){
         perror("dhmiourgia pediou apetixe");
@@ -19,6 +24,23 @@ int main(){
     } else { //kwdikas tou patera
         wait(&status);
         printf("[PARENT] getpid()= %d, getppid()=%d\n",getpid(),getppid());
+
+        int fd = open(argv[1], O_CREAT | O_APPEND | O_WRONLY, 0644); //auto edw einai gia thn eggrafh sto arxeio 
+        if (fd == -1){ //prepei na einai mesa sto parent process giati mono tote tha grapsw sto arxeio
+            perror("open");
+            return 1;
+        }
+        if (write(fd, buf, strlen(buf)) < strlen(buf)){
+            perror("write");
+            return 1;
+        }
+        close(fd);
+        
         exit(0); 
             }
+
+        
+    
+
+    return 0;
 }
