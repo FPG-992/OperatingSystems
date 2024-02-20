277f7a3560ad26a4518eb271b6208d3f7e7b5bc7
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-02-20 11:51:48 +0200

    this is the main program untill now

---
+++ lab_exercises/exercise_1/solution_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(){
+    int status;
+    pid_t child;
+    child = fork();
+
+    if (child<0){
+        printf("dhmiourgia pediou apetixe");
+    }   else if (child==0){
+        pid_t parent_pid;
+        parent_pid = getppid();
+        printf("[CHILD] getpid()= %d, getppid()=%d\n",getpid(),getppid());
+        exit(0);
+    } else { //kwdikas tou patera
+        printf("[PARENT] getpid()= %d, getppid()=%d\n",getpid(),getppid());
+        wait(&status);
+        exit(0); 
+            }
+}
