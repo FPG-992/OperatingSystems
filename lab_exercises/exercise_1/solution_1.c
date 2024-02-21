0e49e4886771f332bf201189d544643bbb10ae5c
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-02-21 17:47:22 +0200

    dieukrinish 1 , 2 , 3 done , auto pou menei einai to redirect tou output apo to printf sto arxeio me write

--- lab_exercises/exercise_1/solution_1.c
+++ lab_exercises/exercise_1/solution_1.c
@@ -5,8 +5,28 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <string.h>
 
+bool file_exists(const char *filename){ //exw ena bool gia na elegxw an to arxeio yparxei
+    struct stat buffer; //to struct stat einai ena struct pou periexei plirofories gia to arxeio
+    return stat(filename,&buffer)==0 ? true : false; //to stat epistrefei 0 an to arxeio yparxei ternary conditional operator
+}
 int main(int argc, char *argv[]){
+    const char *filename = argv[1];
+//dieukrinisi 1,2,3
+if (argc <2 || argc > 2){ //ligotero apo 1 argument kai perissotero apo 1 argument, default to argc einai 1
+    printf("Usage: ./a.out filename");
+    return 1; 
+    } else if (file_exists(filename)==true){ //an to arxeio yparxei
+        printf("Error: %s already exists\n",argv[1]);
+        return 1;
+    } else if (strcmp(argv[1], "--help") == 0) {
+    printf("Usage: ./a.out filename\n");
+    return 0;
+}
+//dieukrinish 3
     int status;
     pid_t child;
     child = fork();
