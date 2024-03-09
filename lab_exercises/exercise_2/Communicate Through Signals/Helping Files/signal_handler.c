07a900d1a0929a2de0706f35f20f74c96d7d9c6f
Peter Jacob <128411298+PJF9@users.noreply.github.com>
2024-03-09 13:19:23 +0200

    Create signal_handler.c

---
+++ lab_exercises/exercise_2/Communicate Through Signals/Helping Files/signal_handler.c
@@ -0,0 +1,33 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
+#include <signal.h>
+#include <strings.h>
+
+
+// Signal handler for SIGINT
+void sigint_handler(int signal) {
+	if (signal == SIGINT) {
+        printf("\nIntercepted SIGINT!\n");
+    }
+}
+
+void set_signal_action(void) {
+	// Declare the sigaction structure
+	struct sigaction act;
+
+	// Set all of the structure's bits to 0 to avoid errors relating to uninitialized variables...
+	bzero(&act, sizeof(act));
+	// Set the signal handler as the default action
+	act.sa_handler = &sigint_handler;
+	// Apply the action in the structure to the SIGINT signal (ctrl-c)
+	sigaction(SIGINT, &act, NULL);
+}
+
+int	main() {
+	set_signal_action();
+
+	// Infinite loop to give us time to do ctrl-c as much as we want!
+	while(1) continue;
+	return 0;
+}
