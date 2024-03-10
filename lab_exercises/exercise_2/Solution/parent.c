c1dc9c18b384faf1b98766471a015e76f5a26f88
Peter Jacob <128411298+PJF9@users.noreply.github.com>
2024-03-10 21:01:37 +0200

    Update parent.c

--- lab_exercises/exercise_2/Solution/parent.c
+++ lab_exercises/exercise_2/Solution/parent.c
@@ -10,6 +10,24 @@
 
 
 
+/*
+    Εργαλεια
+
+    When a child process stops or terminates, SIGCHLD is sent to the parent process.
+    The default response to the signal is to ignore it. The signal can be caught and
+    the exit status from the child process can be obtained by immediately calling wait.
+
+    The exec family of functions replaces the current running process with a new
+    process. It can be used to run a C program by using another C program.
+    All statements are ignored after execvp() call as this whole process(execDemo.c)
+    is replaced by another process (EXEC.c).
+        int execv(const char *path, char *const argv[]);
+    - path: should point to the path of the file being executed. 
+    - argv[]: is a null terminated array of character pointers.
+*/
+
+
+
 // Declearing them as global, because we need them inside `sig_handler`
 pid_t child;
 pid_t pid;
@@ -22,6 +40,7 @@ int wait_status;
 int find;
 
 
+// This function creates a child process and saving some pids for later use
 int create_child(pid_t *child, pid_t *pid, pid_t *ppid, int index, char state) {
     *child = fork();
     *pid = getpid();
@@ -29,7 +48,7 @@ int create_child(pid_t *child, pid_t *pid, pid_t *ppid, int index, char state) {
 
     if (*child < 0) {
         printf("Error: Couldn't create the child-process\n");
-        return -1;
+        return 1;
     }
 
     // Capturing Children
@@ -48,7 +67,7 @@ int create_child(pid_t *child, pid_t *pid, pid_t *ppid, int index, char state) {
         char *args[] = {child_procees, child_id, &child_state, NULL};
         if (execv(child_procees, args) == -1) {
             printf("Error: child.c file can't be executed.\n");
-            return -1;
+            return 1;
         }
     }
     return 0;
@@ -59,14 +78,18 @@ void sig_handler(int signal) {
     if (signal == SIGCHLD) {
         if (wait_status == 2304) {
             // Child's state was 't'
-            create_child(&child, &pid, &ppid, find, 't');
+            if (create_child(&child, &pid, &ppid, find, 't') == 1) {
+                exit(1);
+            }
             if (child != 0) {
                 printf("[PARENT/PID=%d] Created new child for gate %d (PID %d) and initial state 't'\n", pid, find, child);
             }
         }
         else if (wait_status == 2560) {
             // Child's state was 't'
-            create_child(&child, &pid, &ppid, find, 'f');
+            if (create_child(&child, &pid, &ppid, find, 'f') == 1) {
+                exit(1);
+            }
             if (child != 0) {
                 printf("[PARENT/PID=%d] Created new child for gate %d (PID %d) and initial state 'f'\n", pid, find, child);
             }
@@ -96,6 +119,9 @@ void sig_handler(int signal) {
             kill(children_pids[i], SIGUSR2);
         }
     }
+    else if (signal == SIGSTOP) {
+        // Resuming the process by doing nothing when something trying to pause it.
+    }
 }
 
 void set_signal_action() {
@@ -113,6 +139,7 @@ void set_signal_action() {
     sigaction(SIGUSR1, &act, NULL);
     sigaction(SIGTERM, &act, NULL);
     sigaction(SIGUSR2, &act, NULL);
+    sigaction(SIGSTOP, &act, NULL);
 }
 
 
@@ -147,8 +174,7 @@ int main(int argc, char* argv[]) {
 
     set_signal_action();
     
-    N = strlen(argv[1]); // The number of childer to create
-
+    N = strlen(argv[1]); // The number of children to create
     for (int i=0; i<N; ++i) {
         if (create_child(&child, &pid, &ppid, i, argv[1][i]) == -1) {
             return 1;
@@ -160,7 +186,6 @@ int main(int argc, char* argv[]) {
 
         // If the user want's to sent a signal, they could use the command: `kill -SIGNAM pid`
         // Which has three parts: command | signal type | pid
-
         char command[10];
         char sig[20];
         pid_t sig_pid;
@@ -168,19 +193,21 @@ int main(int argc, char* argv[]) {
         while (1) {
             scanf("%s", command);
 
+            // Check if the command is `kill`, otherwise clear the buffer and try again.
             if (strcmp(command, "kill")) {
                 printf("The system can't undertand '%s'. You can only use 'kill' command. Nothing has been modified.\nUsage: kill -SIGNAL pid\n", command);
             }
             else {
                 scanf("%s", sig);
 
+                // Check if the command is SIGTERM, SIGUSR1, or SIGUSR2, otherwise clear the buffer and try again
                 if (strcmp(sig, "-SIGTERM") && strcmp(sig, "-SIGUSR1") && strcmp(sig, "-SIGUSR2")) {
                     printf("The system only understands 'SIGTERM', 'SIGUSR1', 'SIGUSR2'. Nothing has been modified.\n");
                 }
                 else {
                     scanf("%d", &sig_pid);
 
-                    // Condition if the pid doesn't exists
+                    // Find if the given pid is one of the children's.
                     find = -1;
                     for (int i=0; i<N; ++i) {
                         if (children_pids[i] == sig_pid || sig_pid == pid) {
@@ -192,7 +219,7 @@ int main(int argc, char* argv[]) {
                         printf("The pid you've entered is not a child's, not the parent process. Nothing has been modified.\n");
                     }
                     else {
-                        // Sending the Signals
+                        // The command is valid, so sending signals to the given process
                         if (!strcmp(sig, "-SIGTERM")) {
                             if (kill(sig_pid, SIGTERM) == -1) {
                                 return 1;
