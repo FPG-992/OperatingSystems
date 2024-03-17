53461e40908d2d7ed68b160c7ac4485be22dd215
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-03-18 01:02:41 +0200

    Added sigaction , handler and signal "pipe"
    Need to work on handler and then work on
    adding some variables as global

--- lab_exercises/exercise_2/Solution_Filippos/parent.c
+++ lab_exercises/exercise_2/Solution_Filippos/parent.c
@@ -5,10 +5,56 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #define MAX_CHILDREN 1000000
+#define _XOPEN_SOURCE 700 // resolves the `struct sigaction sa` incomplete type error
 
 void handler(int signum){
-    printf("Signal %d received\n",signum);
+  if (strcmp(signal,"-SIGTERM")==0){
+                if (signal_pid==parent){
+                    for(int i=0; i<N; i++){
+                        kill(child_pid[i],SIGTERM);
+                        printf("Child %d terminated\n",child_pid[i]);
+                    }
+                } else {
+                    kill(signal_pid,SIGTERM);
+                    printf("Child %d terminated\n",signal_pid);
+                }
+            }
+            if (strcmp(signal,"-SIGUSR1")==0){
+                if (signal_pid==parent){
+                    for(int i=0; i<N; i++){
+                        kill(child_pid[i],SIGUSR1);
+                        printf("Child %d received SIGUSR1\n",child_pid[i]);
+                    }
+                } else {
+                    kill(signal_pid,SIGUSR1);
+                    printf("Child %d received SIGUSR1\n",signal_pid);
+                }
+            }
+            if (strcmp(signal,"-SIGUSR2")==0){
+                if (signal_pid==parent){
+                    for(int i=0; i<N; i++){
+                        kill(child_pid[i],SIGUSR2);
+                        printf("Child %d received SIGUSR2\n",child_pid[i]);
+                    }
+                } else {
+                    kill(signal_pid,SIGUSR2);
+                    printf("Child %d received SIGUSR2\n",signal_pid);
+                }
+            }  
 }
+
+void setup_sigaction() {
+    struct sigaction act; // Declare a struct to hold signal action settings
+    bzero(&act, sizeof(act)); // Initialize all members of the struct to zero
+    act.sa_handler = handler; // Set the handler function to be our defined function
+    //apply actions to the signals
+    sigaction(SIGCHLD, &act, NULL);
+    sigaction(SIGUSR1, &act, NULL);
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGUSR2, &act, NULL);
+    sigaction(SIGSTOP, &act, NULL);
+}
+
 
 int main(int argc, char* argv[]){
 pid_t parent = getpid();
@@ -18,6 +64,8 @@ char command[10];
 char signal[10];
 pid_t find;
 char index[10]; //buffer to store the integer value of i
+//enabling sigaction
+setup_sigaction();
 //check for argc, argv input errors
 if (argv[1] != NULL && strcmp(argv[1], "--help") == 0) { //error exception because !=NULL was not in the if statement
     printf("Usage: ./gates Nstates\n");
@@ -106,6 +154,29 @@ while(1){
             break;
         }else{
             if (strcmp(signal,"-SIGTERM")==0){
+                if (kill(signal_pid,SIGTERM)==-1){
+                    perror("Signal not sent\n");
+                } 
+            if (strcmp(signal,"-SIGUSR1")==0){
+                if (kill(signal_pid,SIGUSR1)==-1){
+                    perror("Signal not sent\n");
+                }
+            }
+            if (strcmp(signal,"-SIGUSR2")==0){
+                if (kill(signal_pid,SIGUSR2)==-1){
+                    perror("Signal not sent\n");
+                }
+            }
+        }
+}
+           
+    //clearing input buffer, Credits:PJF
+    while (getchar() != '\n'){printf("Enter new command:\n");}
+
+}
+
+/* function for executing the commands
+if (strcmp(signal,"-SIGTERM")==0){
                 if (signal_pid==parent){
                     for(int i=0; i<N; i++){
                         kill(child_pid[i],SIGTERM);
@@ -138,12 +209,4 @@ while(1){
                     printf("Child %d received SIGUSR2\n",signal_pid);
                 }
             }
-        }
-        
-        
-}
-           
-    //clearing input buffer, Credits:PJF
-    while (getchar() != '\n'){printf("Enter new command:\n");}
-
-}
+*/
