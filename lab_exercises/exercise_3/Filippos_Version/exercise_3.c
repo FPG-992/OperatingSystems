5e67ade87755c0805fa7e77d7989ad2d7b5e0e70
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-09 18:18:46 +0300

    Added reading arguments from CL
    created makefile, added input.txt to create the
    program in parts

--- lab_exercises/exercise_3/Filippos_Version/exercise_3.c
+++ lab_exercises/exercise_3/Filippos_Version/exercise_3.c
@@ -19,6 +19,21 @@ int main(int argc, char* argv[]){ //argc = number of arguments (1 default), argv
 int N; //ammount of children parent will create
 N=atoi(argv[1]); //argument to intiger function
 
+//start to check for Command Line arguments
+int default_mode = 0; //default mode = round robin (0) or random (1)
+if (argc==2 && N>=1){
+    printf("Default mode: Round Robin\n");
+}else if (argc==3 && N>=1 && (strcmp(argv[2],"--round-robin")==0)){
+    printf("Default mode: Round Robin\n");
+}else if (argc==3 && N>=1 && (strcmp(argv[2],"--random")==0)){
+    default_mode = 1;
+    printf("Default mode: Random\n");
+}else{
+    printf("Usage: ask3 <nChildren> [--random] [--round-robin]\n");
+    return 0;
+}
+
+
 if (pipe(parent_to_child) == -1 || pipe(child_to_parent) == -1){ //create pipes and check for errors simultaneously
     perror("pipe");
     exit(0);
